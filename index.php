9a6a3abccb33c7de8a76c0928593d985f32a6229
dilumsadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2022-03-20 19:03:23 +0530

    file arrenment

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    
+    <?php 
+
+    include "layout/nav.php";
+
+    ?>
+
+    <div class="hero">
+        <div id="slider">
+            <figure>
+            <img src="public/img/hero1.jpg" class="hero-slider" alt>
+            <img src="public/img/hero2.jpg" class="hero-slider" alt>
+            <img src="public/img/hero3.jpg" class="hero-slider" alt>
+            <img src="public/img/hero4.jpg" class="hero-slider" alt>
+            <img src="public/img/hero5.jpg" class="hero-slider" alt>
+            </figure>
+        </div>
+        <div class="hero-dis">
+            <h1>Hotel Indigo</h1>
+            <p>Unlock to Enjoy the View of Martine</p>
+            <br>
+            <a href="" class="btn-info">Book a Room</a>
+
+        </div>
+
+        <div class="hero-book">
+            
+        </div>
+    </div>
+
+
+
+
+
+    <style>
+         
+    </style>
+
+</body>
+</html>
